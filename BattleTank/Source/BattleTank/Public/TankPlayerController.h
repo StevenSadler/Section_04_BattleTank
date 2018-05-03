c3ff5859105b6f17461584ffb3f3e65dee42ba25
Steven Sadler <stevenjsadler@gmail.com>
2018-05-03 07:26:09 -0700

    add blueprint variable to change crosshair color

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -19,6 +19,8 @@ public:
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
 
+protected:
+	UFUNCTION(BlueprintCallable, Category = "Setup")
 	ATank* GetControlledTank() const;
 
 private:
